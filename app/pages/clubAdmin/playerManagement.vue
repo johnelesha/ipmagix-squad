0fe28049655cb74a3e13a0c84b17a45b2d29d800
John <jo55.elesha@gmail.com>
2025-08-30 13:26:04 +0300

    edit in logo icon and add new link in sidebar

---
+++ app/pages/clubAdmin/playerManagement.vue
@@ -0,0 +1,13 @@
+<template>
+    <div>
+        <h2>Player Management</h2>
+    </div>
+</template>
+
+<script setup>
+definePageMeta({
+    layout: "clubs-admin",
+});
+</script>
+
+<style></style>
